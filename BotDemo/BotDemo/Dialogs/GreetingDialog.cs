5f5987983c3d8792c87e604e747256df75d03e81
lschlotthauer <lucas6sic6@gmail.com>
2017-05-19 13:00:31 -0300

    IDialog

---
+++ BotDemo/BotDemo/Dialogs/GreetingDialog.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Microsoft.Bot.Connector;
+
+namespace BotDemo.Dialogs
+{
+    [Serializable]
+    public class GreetingDialog : IDialog
+    {
+        public async Task StartAsync(IDialogContext context)
+        {
+            await context.PostAsync("Hi I'm John Bot");
+            context.Wait(MessageRecievedAsync);
+        }
+
+        public virtual async Task MessageRecievedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
+        {
+            var message = await argument;
+            var userName = String.Empty;
+            var getName = false;
+            context.UserData.TryGetValue<string>("Name", out userName);
+            context.UserData.TryGetValue<bool>("GetName", out getName);
+
+            if (getName)
+            {
+                userName = message.Text;
+                context.UserData.SetValue<string>("Name", userName);
+                context.UserData.SetValue<bool>("GetName", false);
+            }
+
+            if (string.IsNullOrEmpty(userName))
+            {
+                await context.PostAsync("What is your name?");
+                context.UserData.SetValue<bool>("GetName", true);
+            }
+            else
+            {
+                await context.PostAsync($"Hi {userName}. How can I help you today?");
+            }
+            context.Wait(MessageRecievedAsync);
+        }
+    }
+}
